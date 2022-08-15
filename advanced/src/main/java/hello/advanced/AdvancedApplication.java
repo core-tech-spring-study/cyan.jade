7808c3f86df876fecf871c104c7bc140e5f0656e
jade.cyan <cyanjd1226@gmail.com>
2022-08-16 00:03:07 +0900

    commit 확인

--- advanced/src/main/java/hello/advanced/AdvancedApplication.java
+++ advanced/src/main/java/hello/advanced/AdvancedApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdvancedApplication {
+public class AdvancedApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
